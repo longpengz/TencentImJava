a625212319bb6797752225751e3e2c5fcb0c60b7
longpeng <2916879310@qq.com>
2020-12-12 14:53:00 +0800

    first commit

---
+++ src/test/java/com/longpengz/tencentim/TencentimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.longpengz.tencentim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TencentimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
