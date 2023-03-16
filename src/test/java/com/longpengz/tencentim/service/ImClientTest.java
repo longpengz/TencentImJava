1bb0204dcd5336cd95efd0339cb8e1abcb6fba8e
longpengZ <longpengz@foxmail.com>
2023-03-16 11:42:09 +0800

    feat：增加测试

---
+++ src/test/java/com/longpengz/tencentim/service/ImClientTest.java
@@ -0,0 +1,12 @@
+package com.longpengz.tencentim.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ImClientTest {
+
+    @Test
+    void init() {
+    }
+}
---
+++ src/test/java/com/longpengz/tencentim/service/ImFactoryTest.java
@@ -0,0 +1,32 @@
+package com.longpengz.tencentim.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ImFactoryTest {
+
+    @Test
+    void getAccountService() {
+    }
+
+    @Test
+    void getSingleChatService() {
+    }
+
+    @Test
+    void getGroupOpenHttpSvcService() {
+    }
+
+    @Test
+    void getOpenconfigsvrService() {
+    }
+
+    @Test
+    void getProfileService() {
+    }
+
+    @Test
+    void getSnsService() {
+    }
+}
